7bd1dd86a395073b876c6ddbb0d63fd9d6aa7fed
unknown <m.ali2910@gmail.com>
2020-02-19 21:18:52 +0200

    logger

--- src/helpers/mylogger.py
+++ src/helpers/mylogger.py
@@ -1,17 +1,22 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import sys
+import datetime
 
 def create_logger(name):
-    # name
-    mylogger = logging.getLogger(name)
-    # level
-    mylogger.setLevel(logging.DEBUG)
-    # file
-    fh = logging.FileHandler('logs.log')
-    fh.setLevel(logging.DEBUG)
-    # format
+    # log path
+    logFile = 'storage/logs/logs_'+datetime.datetime.now().strftime("%Y_%m_%d")+'.log'
+    my_handler = RotatingFileHandler(logFile)
+    # set format
     formatter = logging.Formatter("""%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(funcName)s():line %(lineno)d  - %(relativeCreated)6d - %(threadName)s :\n %(message)s\n""")
-    fh.setFormatter(formatter)
-    mylogger.addHandler(fh)
-    
+    my_handler.setFormatter(formatter)
+    # logger name
+    mylogger = logging.getLogger(name)
+    # logger level
+    mylogger.setLevel(logging.DEBUG)
+    # add file handler
+    mylogger.addHandler(my_handler)
+    # add console handler
+    mylogger.addHandler(logging.StreamHandler(sys.stdout))
     return mylogger
    
